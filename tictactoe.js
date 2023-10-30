19602c3616b81195554b571e50210bf81187f33e
Conor Kay <conorkay1@gmail.com>
2023-10-29 23:07:28 -0400

    Begin implementation of game logic.

--- tictactoe.js
+++ tictactoe.js
@@ -1,9 +1,19 @@
 const gameBoard = (function () {
   var gameBoardArray = new Array(9);
 
-  const resetGameBoard = () => (gameBoardArray = [, , , , , , , ,]);
+  const updateGameBoard = () => {
+    for (let i = 0; i < 9; i++) {
+      let elementID = 'tile' + (i + 1);
+      let element = document.getElementById(elementID);
+      element.innerText = gameBoardArray[i];
+    }
+  };
 
-  return { gameBoardArray, resetGameBoard };
+  const resetGameBoard = () => (gameBoardArray = [, , , , , , , ,]);
+  const getArray = () => gameBoardArray;
+  const updateArray = (index, symbol) => (gameBoardArray[index] = symbol);
+
+  return { getArray, resetGameBoard, updateGameBoard, updateArray };
 })();
 
 const game = (function () {
@@ -12,15 +22,22 @@ const game = (function () {
   const isGameOver = () => isOver;
   const changeGameStatus = () => (isOver = !isOver);
 
+  const player1 = createPlayer(1);
+  const player2 = createPlayer(2);
+  gameBoard;
+
   return { isGameOver, changeGameStatus };
 })();
 
-function createPlayer(name) {
-  let playerName = name;
+const displayController = (function () {})();
+
+function createPlayer(id) {
+  let playerID = id;
   let score = 0;
   const win = () => ++score;
   const getScore = () => score;
   const resetScore = () => (score = 0);
+  const getId = () => playerID;
 
-  return { playerName, win, getScore, resetScore };
+  return { getId, win, getScore, resetScore };
 }
