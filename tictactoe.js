9d5d1fe3de1aa737a2ff5757d52074994f42c2fd
Conor Kay <conorkay1@gmail.com>
2023-11-02 12:08:01 -0400

    Implement win conditions

--- tictactoe.js
+++ tictactoe.js
@@ -21,6 +21,9 @@ const gameBoard = (function () {
         element.innerText = gameBoardArray[i];
       }
     }
+    if (gameBoard.checkWinCondition()) {
+      displayController.removeLoop();
+    }
   };
 
   const resetGameArray = () => (gameBoardArray = [, , , , , , , ,]);
@@ -34,7 +37,46 @@ const gameBoard = (function () {
     }
   };
 
-  const checkWinCondition = () => {};
+  const checkWinCondition = () => {
+    if (
+      // horizontal rows
+      (gameBoardArray[0] === gameBoardArray[1] &&
+        gameBoardArray[1] === gameBoardArray[2] &&
+        gameBoardArray[0] != undefined) ||
+      (gameBoardArray[3] === gameBoardArray[4] &&
+        gameBoardArray[4] === gameBoardArray[5] &&
+        gameBoardArray[3] != undefined) ||
+      (gameBoardArray[6] === gameBoardArray[7] &&
+        gameBoardArray[7] === gameBoardArray[8] &&
+        gameBoardArray[6] != undefined) ||
+      // vertical columns
+      (gameBoardArray[0] === gameBoardArray[3] &&
+        gameBoardArray[3] === gameBoardArray[6] &&
+        gameBoardArray[0] != undefined) ||
+      (gameBoardArray[1] === gameBoardArray[4] &&
+        gameBoardArray[4] === gameBoardArray[7] &&
+        gameBoardArray[1] != undefined) ||
+      (gameBoardArray[2] === gameBoardArray[5] &&
+        gameBoardArray[5] === gameBoardArray[8] &&
+        gameBoardArray[2] != undefined) ||
+      // diagonals
+      (gameBoardArray[0] === gameBoardArray[4] &&
+        gameBoardArray[4] === gameBoardArray[8] &&
+        gameBoardArray[0] != undefined) ||
+      (gameBoardArray[2] === gameBoardArray[4] &&
+        gameBoardArray[4] === gameBoardArray[6] &&
+        gameBoardArray[2] != undefined)
+    ) {
+      game.changeGameStatus();
+      game.processWin();
+      console.log('Winning turn #' + game.getCurrentTurn());
+      console.log('game over');
+      console.log(game.isGameOver());
+      displayController.removeLoop();
+      return true;
+    }
+    return false;
+  };
 
   return {
     getArray,
@@ -47,13 +89,14 @@ const gameBoard = (function () {
 
 const game = (function () {
   var isOver = false;
-  var turnCount = 0;
+  var turnCount = 1;
   var currentTurn = 1;
+  var winner = 0;
 
   const isGameOver = () => isOver;
   const changeGameStatus = () => (isOver = !isOver);
   const getTurnCount = () => turnCount;
-  const incrementTurn = () => turnCount++;
+  const incrementTurnCount = () => turnCount++;
   const getCurrentTurn = () => currentTurn;
   const changeTurn = () => {
     if (currentTurn === 1) {
@@ -69,15 +112,32 @@ const game = (function () {
   const start = () => {
     while (!isOver) {}
   };
+
   const reset = () => {
     isOver = false;
-    turnCount = 0;
+    turnCount = 1;
     currentTurn = 1;
     gameBoard.resetGameArray();
     gameBoard.updateGameBoard();
     player1.resetScore();
     player2.resetScore();
-    //newGame = true;
+    newGame = true;
+    displayController.removeLoop();
+  };
+
+  const processTie = () => {
+    game.changeGameStatus();
+    console.log('its a tie');
+    game.reset();
+  };
+
+  const processWin = () => {
+    if (currentTurn === 1) {
+      winner = 'x';
+    } else {
+      winner = 'o';
+    }
+    console.log('winner: ' + winner);
   };
 
   return {
@@ -88,54 +148,61 @@ const game = (function () {
     getTurnCount,
     getCurrentTurn,
     changeTurn,
-    incrementTurn,
+    incrementTurnCount,
+    processTie,
+    processWin,
   };
 })();
 
 const displayController = (function () {
   var elemArray = new Array();
+  const tile0 = document.getElementById('0');
+  const tile1 = document.getElementById('1');
+  const tile2 = document.getElementById('2');
+  const tile3 = document.getElementById('3');
+  const tile4 = document.getElementById('4');
+  const tile5 = document.getElementById('5');
+  const tile6 = document.getElementById('6');
+  const tile7 = document.getElementById('7');
+  const tile8 = document.getElementById('8');
+  elemArray.push(tile0, tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8);
 
   const initialize = () => {
-    const tile0 = document.getElementById('0');
-    const tile1 = document.getElementById('1');
-    const tile2 = document.getElementById('2');
-    const tile3 = document.getElementById('3');
-    const tile4 = document.getElementById('4');
-    const tile5 = document.getElementById('5');
-    const tile6 = document.getElementById('6');
-    const tile7 = document.getElementById('7');
-    const tile8 = document.getElementById('8');
-    elemArray.push(
-      tile0,
-      tile1,
-      tile2,
-      tile3,
-      tile4,
-      tile5,
-      tile6,
-      tile7,
-      tile8
-    );
     elemArray.forEach(addListener);
   };
   const addListener = (elem) => {
-    elem.addEventListener('click', (event) => {
-      var success;
-      if (game.getCurrentTurn() === 1) {
-        success = gameBoard.updateArray(elemArray.indexOf(elem), 'x');
-      } else {
-        success = gameBoard.updateArray(elemArray.indexOf(elem), 'o');
-      }
-      if (success) {
-        console.log('success');
-        gameBoard.updateGameBoard();
-        game.changeTurn();
-        game.incrementTurn();
-      }
-    });
+    elem.addEventListener('click', handleEvent);
   };
 
-  return { initialize };
+  function handleEvent(e) {
+    var success;
+
+    if (game.getCurrentTurn() === 1) {
+      success = gameBoard.updateArray(elemArray.indexOf(e.currentTarget), 'x');
+    } else {
+      success = gameBoard.updateArray(elemArray.indexOf(e.currentTarget), 'o');
+    }
+    if (success) {
+      console.log('success');
+      gameBoard.updateGameBoard();
+      game.changeTurn();
+      game.incrementTurnCount();
+      console.log(game.getTurnCount());
+      if (game.getTurnCount() === 10) {
+        game.processTie();
+      }
+    }
+  }
+
+  const removeLoop = () => {
+    elemArray.forEach(removeListener);
+  };
+
+  const removeListener = (elem) => {
+    elem.removeEventListener('click', handleEvent);
+  };
+
+  return { initialize, removeLoop };
 })();
 
 function createPlayer(id) {
